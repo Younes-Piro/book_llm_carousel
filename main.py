dc02637220cd3f5781f85a97e5dde62120d7dbe7
Younes-Piro <piroyounes148@gmail.com>
2024-03-21 11:13:44 +0000

    store embed fixed

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.docstore.document import Document
 from utils import helpers
-import re
+from chromadb.utils import embedding_functions
+import chromadb
+import uuid
 
 book = './data/Cutting-edge+digital+marketing.pdf'
+path = './chromadb'
+name_collection = 'book_emb'
 
 # read the book
 book_text = helpers.read_pdf_path(book)
@@ -23,6 +27,24 @@ book_docs = [
 ]
 ## split into chunks
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=150)
-docs = text_splitter.split_documents(book_docs)
+chunks = text_splitter.split_documents(book_docs)
+docs = [x.page_content for x in chunks]
 
-print(docs)
+## embed and store into chroma db
+ef = embedding_functions.InstructorEmbeddingFunction(model_name="hkunlp/instructor-base") 
+
+## initialise chroma db client
+client = chromadb.PersistentClient(path=path)
+collection = client.get_or_create_collection(
+            name=name_collection,
+            embedding_function=ef,
+            metadata={"hnsw:space": "cosine"}
+        )
+
+print("## START EMBEDDING FUNCT")
+collection.add(
+            documents=docs,
+            ids=[str(uuid.uuid4()) for _ in range(len(docs))]
+        )
+        
+print("EMB DONE")
