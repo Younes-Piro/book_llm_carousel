81db0d9f322776f9915e9ebc28a28dc4bfc006a3
Younes-Piro <piroyounes148@gmail.com>
2024-03-25 10:26:22 +0000

    streamlit app init

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import streamlit as st
 from models.carousel_generator import generate_carousel
 
-query = 'how to maximum ROI?'
+def main():
+    st.title('StreamIt App')
+    
+    # Input box for user input
+    user_input = st.text_input('Enter your question :')
+    
+    if st.button('Process'):
+        if user_input:
+            try:
+                query = str(user_input)
+                result = generate_carousel(query)
+                st.success(f'Result: {result}')
+            except ValueError:
+                st.error('Please enter a valid number.')
+        else:
+            st.warning('Please enter a number above.')
 
-result = generate_carousel(query)
-
-print(result)
+if __name__ == '__main__':
+    main()
